c44777a931ae04403b769005799d4305dfe621ff
John Arne Øye <johnarneoye@gmail.com>
2013-02-11 22:32:50 +0100

    CHANGED EVERYtHING!

--- EA.py
+++ EA.py
@@ -1,20 +1,21 @@
 
 import sys
 from FitnessEval import FitnessEval
-from EAOperators import Selection
 from EAOperators import Individual
 from Plotting import Plotting
+import random
 
 class EA:
     
     population_size = 20 #Size of the population
     generations = 200 #Number of generations
     generation = 0 #Current generation nr
-    nr_of_bits = 20 #Bitlength 
+    fitness_goal = 40 #The fitness goal
     k = 4 #Group size in k_tournament
     e = 0.2 #Probability of selecting random in k_tournament
     mutation_probability = 0.8 #Probability that mutation of a specimen will occur
     mutation_count = 1 #Number of bits mutated when mutating
+
     population_genotype = []
     population_fitness = []
     new_generation = []
@@ -41,55 +42,144 @@ class EA:
         for p in self.population:
             p.development()
     
-    
     def select(self):
         self.population_fitness = []
         for p in self.population:
             p.calc_fitness()
             
         best_individual = self.sorted_population()[0]
-        if best_individual.fitness == self.nr_of_bits:
+        if best_individual.fitness == self.fitness_goal:
             print "SOLUTION FOUND: "+str(best_individual.phenotype)+ " " +str( best_individual.fitness )
             sys.exit()
             
-        if((self.generation%10) == 0):
+        if((self.generation%2) == 0):
             print "GENERATION:: " +str(self.generation)
-            print "Max fitness: " +str(best_individual.fitness) +": " + best_individual.phenotype
+            print "Max fitness: " +str(best_individual.fitness) +": " + str(best_individual.phenotype)
             print "Avg fitness: " +str( (self.sum_population()/len(self.population)) )
         
-        self.reproducers = self.selects.k_tournament(self.population, self.k, self.e)
+        self.reproducers = self.selects.fitness_proportionate(self.population, self.sum_population())
+
     
     def reproduce(self):
         self.children = []
-        for _ in range(0, self.k):
-            for p in self.reproducers:
-                self.children.append(p)
-        print self.children
+        for p in self.reproducers:
+            self.children.append(p.crossover( self.reproducers[random.randint(0,len(self.reproducers)-1)] ))
     
     def operate(self):
         for p in self.children:
             p.mutate(self.mutation_probability)
         
-        for p in self.children:
-            p.crossover()
-    
     def replace(self):
-        new_generation = []
-        for i in self.mutated_genotypes:
-            new_generation.append(self.develops.development(i, self.nr_of_bits))
+        for p in self.children:
+            p.development()
         self.generation += 1
-        self.population = Selection().full_gen_replacement(self.population, new_generation)
-        self.population_genotype = self.children_genotypes
-        
-    
+        self.population = Selection().full_gen_replacement(self.population, self.children)
+          
     def sorted_population(self):
         return sorted(self.population, lambda x, y: cmp(x.fitness, y.fitness))[::-1]
+    
+    def sorted_children(self):
+        return sorted(self.children, lambda x, y: cmp(x.fitness, y.fitness))[::-1]        
 
     def sum_population(self):
         fitness_sum = 0
         for p in self.population:
             fitness_sum += p.fitness
         return fitness_sum
+    
+#Contains the different selection protocols
+#and selection mechanisms  
+class Selection:
+    
+    #Set which selection protocol to use
+    def set_selection_protocol(self):
+        return False
+        
+    #SELECTION PROTOCOLS
+    def full_gen_replacement(self, population, children):
+        return children
+        
+    def over_production(self):
+        return False
+    
+    def generational_mixing(self):
+        return False
+        
+        
+        
+    #Set which selection mechanism to use
+    def set_selecton_mechanism(self):
+        return False
+    
+    #SELECTION MECHANISMS    
+    #Fitness proportionate scaling of fitness and spins the wheel
+    def fitness_proportionate(self, population, sum_fitness):
+        expected_mating = []
+        average_fitness = sum_fitness/len(population)
+        mating_wheel = []
+        for p in population:
+            expected_mating = int(round(p.fitness/average_fitness))
+            for _ in range(0, expected_mating):
+                mating_wheel.append(p) 
+            
+        #THEN SPIN ZE WHEEEEL
+        reproducers = []
+        for _ in range(0, len(population)):
+            reproducers.append( mating_wheel[random.randint(0,len(mating_wheel)-1)] )
+        
+        return reproducers
+       
+    #Sigma scaling of fitness and spins the wheel 
+    def sigma_scaling(self, population, sum_fitness):
+        expected_mating = []
+        average_fitness = sum_fitness/len(population)
+        standard_deviation = sum( map(lambda x: (x - average_fitness)**2, sum_fitness) )
+        mating_wheel = []
+        for p in population:
+            expected_mating = int(1 + ( (p.fitness-average_fitness) / 2*standard_deviation ))
+            for _ in range(0, expected_mating):
+                mating_wheel.append(p) #Indexes
+        #THEN SPIN ZE WHEEEEL
+        reproducers = []
+        for _ in range(0, len(population)):
+            reproducers.append( mating_wheel[random.randint(0,len(mating_wheel)-1)] )
+        
+        return reproducers
+        
+    #Rank scaling of fitness and spins the wheel
+    #TODO
+    def rank(self, population_fitness, sum_fitness):
+        expected_mating = []
+        average_fitness = sum(population_fitness)/len(population_fitness)
+        mating_wheel = []
+        for i in range(0, population_fitness):
+            expected_mating = min + (max-min)*()
+            for _ in range(0, expected_mating):
+                mating_wheel.append(i) #Indexes
+            
+        #THEN SPIN ZE WHEEEEL
+        reproducers = []
+        for _ in range(0, len(population_fitness)):
+            reproducers.append( mating_wheel[random.randint(0,len(mating_wheel)-1)] )
+        
+        return reproducers
+    
+
+    #Selects the index of the reproducers in the population by means of local k-tournament, currently only works on popsizes divisible by k
+    def k_tournament(self, population, k, e):
+        reproducers = []
+        group_k = k
+        for i in range(0, len(population)):
+            if i == group_k-1:
+                #FACE-OFF!
+                tournament_group = population[(i-(k-1)):group_k]
+                for _ in tournament_group:
+                    if random.random()<e:
+                        reproducers.append( population[random.randint(0, k-1) + (group_k-k)] )
+                    else:
+                        reproducers.append( population[tournament_group.index(max(tournament_group)) + (group_k-k)] )
+                group_k = group_k + k
+        return reproducers
     
 if __name__ == '__main__':
     ea = EA()
--- EAOperators.py
+++ EAOperators.py
@@ -4,40 +4,48 @@ import sys
 
 #A Individual in the population
 class Individual:
-    genotype = None
-    phenotype = []
-    nr_of_bits = 20
-    fitness = 0
+
+    nr_of_bits = 40
     
     def __init__(self, genotype=None):
+        self.fitness = 0
+        self.phenotype = []
         if genotype is None:
-            genotype = Individual.random_genotype()
+            self.random_genotype()
+        else:
+            self.genotype=genotype
+            
+    def __str__(self):
+        return str(self.phenotype)
         
+    def __repr__(self):
+        return self.__str__()
+    
     def random_genotype(self):
         self.genotype = random.getrandbits(self.nr_of_bits)
                  
     #Mutate the genotypes, with a probability of mutation_prob  
     def mutate(self, mutation_prob):
-        if random.random() < self.mutation_prob:
-            mutated_genotype = self.genotype ^ (1 << random.randint(0, self.nr_of_bits))
+        if random.random() < mutation_prob:
+            self.genotype = self.genotype ^ (1 << random.randint(0, self.nr_of_bits))
         else:
-            mutated_genotype = self.genotype
-        return mutated_genotype
+            self.genotype = self.genotype
+        return self.genotype
         
     #Perform crossover on genotypes
-    def crossover(self, individual2, crossover_rate):
+    def crossover(self, other):
         crossover_range = (2, 5)
-        splits = [(i % 2, random.randint(*crossover_range)) for i in range(self.genotype_length / crossover_range[0])]
+        splits = [(i % 2, random.randint(*crossover_range)) for i in range(self.nr_of_bits / crossover_range[0])]
         
-        genotypes = (num_to_bitstring(self.genotype), num_to_bitstring(other.genotype))
+        genotypes = (self.num_to_bitstring(self.genotype), self.num_to_bitstring(other.genotype))
         
         new_genotype = []
         index = 0
         for individual, n_genes in splits:
-            to_index = min(index+n_genes, self.genotype_length)
+            to_index = min(index+n_genes, self.nr_of_bits)
             new_genotype.append(genotypes[individual][index:to_index])
             
-            if to_index >= self.genotype_length:
+            if to_index >= self.nr_of_bits:
                 break
             
             index += n_genes
@@ -48,6 +56,7 @@ class Individual:
     #Develop the individual from genotype to phenotype  
     def development(self):
         gtype = int(self.genotype)
+        
         for _ in range(0, self.nr_of_bits):
             self.phenotype.insert(0, gtype % 2)
             gtype = gtype/2
@@ -55,106 +64,13 @@ class Individual:
     def calc_fitness(self):
         self.fitness = FitnessEval().calc_fitness(self.phenotype)
 
+    def num_to_bitstring(self, n, l=20):
+        return bin(n)[2:].zfill(l)
         
 
-#Contains the different selection protocols
-#and selection mechanisms  
-class Selection:
-    
-
-    #Set which selection protocol to use
-    def set_selection_protocol(self):
-        return False
-        
-    #SELECTION PROTOCOLS
-    def full_gen_replacement(self, population, new_generation):
-        return new_generation
-        
-    def over_production(self):
-        return False
-    
-    def generational_mixing(self):
-        return False
-        
-        
-        
-    #Set which selection mechanism to use
-    def set_selecton_mechanism(self):
-        return False
-    
-    #SELECTION MECHANISMS    
-    #Fitness proportionate scaling of fitness and spins the wheel
-    def fitness_proportionate(self, population, sum_fitness):
-        expected_mating = []
-        average_fitness = sum_fitness/len(population)
-        mating_wheel = []
-        for p in range(0, population):
-            expected_mating = round(p.fitness/average_fitness)
-            for _ in range(0, expected_mating):
-                mating_wheel.append(p) 
-            
-        #THEN SPIN ZE WHEEEEL
-        reproducers = []
-        for _ in range(0, len(population)):
-            reproducers.append( mating_wheel[random.randint(0,len(mating_wheel)-1)] )
-        
-        return reproducers
-       
-    #Sigma scaling of fitness and spins the wheel 
-    def sigma_scaling(self, population, sum_fitness):
-        expected_mating = []
-        average_fitness = sum_fitness/len(population)
-        standard_deviation = sum( map(lambda x: (x - average_fitness)**2, sum_fitness) )
-        mating_wheel = []
-        for p in range(0, population):
-            expected_mating = 1 + ( (p.fitness-average_fitness) / 2*standard_deviation )
-            for _ in range(0, expected_mating):
-                mating_wheel.append(p) #Indexes
-        #THEN SPIN ZE WHEEEEL
-        reproducers = []
-        for _ in range(0, len(population)):
-            reproducers.append( mating_wheel[random.randint(0,len(mating_wheel)-1)] )
-        
-        return reproducers
-        
-    #Rank scaling of fitness and spins the wheel
-    #TODO
-    def rank(self, population_fitness, sum_fitness):
-        expected_mating = []
-        average_fitness = sum(population_fitness)/len(population_fitness)
-        mating_wheel = []
-        for i in range(0, population_fitness):
-            expected_mating = min + (max-min)*()
-            for _ in range(0, expected_mating):
-                mating_wheel.append(i) #Indexes
-            
-        #THEN SPIN ZE WHEEEEL
-        reproducers = []
-        for _ in range(0, len(population_fitness)):
-            reproducers.append( mating_wheel[random.randint(0,len(mating_wheel)-1)] )
-        
-        return reproducers
-    
-
-    #Selects the index of the reproducers in the population by means of local k-tournament, currently only works on popsizes divisible by k
-    def k_tournament(self, population, k, e):
-        reproducers = []
-        group_k = k
-        for i in range(0, len(population)):
-            if i == group_k-1:
-                #FACE-OFF!
-                tournament_group = population[(i-(k-1)):group_k]
-                if random.random()<e:
-                    reproducers.append( population[random.randint(0, k-1) + (group_k-k)] )
-                else:
-                    reproducers.append( population[tournament_group.index(max(tournament_group)) + (group_k-k)] )
-                group_k = group_k + k
-        return reproducers
-        
-
-if __name__ == '__main__':
-    
-
+#if __name__ == '__main__':
+#    
+#    
 #    # 1. Create initial random population, develop to phenotype
 #    # 2. Evaluate the fitness of each individual in the Population, do we have solution?
 #    # 3. Select the individuals to reproduce
@@ -214,5 +130,3 @@ if __name__ == '__main__':
 #        population_genotype = children_genotypes
 #        
 #        #START OVER MAKE A FOR FFS! GTG!
-
-    
